b00f135d1fc44315e608b92b16085c871c0e1ef1
wighthouse <44740047+wighthouse@users.noreply.github.com>
2019-02-16 21:27:11 -0800

    Update toggle-menu.js

--- assignments/lesson-6/toggle-menu.js
+++ assignments/lesson-6/toggle-menu.js
@@ -1,3 +1,3 @@
 function toggleMenu() {
-	document.getElementsByClassName("nav")[0].classList.toggle("responsive");
+	document.getElementsByClassName("navigation")[0].classList.toggle("responsive");
 }
